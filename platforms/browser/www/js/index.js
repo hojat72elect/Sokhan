3dfb3334fa9e60eb4c1e3d1075958376741776cc
Hojat <hojat72elect@gmail.com>
2024-12-02 14:23:55 -0500

    Added the initial UI for dictionary.

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -2,9 +2,29 @@
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
+
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+
+    // Get the search button and input element
+    const searchButton = document.getElementById('search-button');
+    const searchBar = document.getElementById('search-bar');
+
+    /**
+     * will be called on each search.
+     */
+    function handleSearch() {
+        console.log("The input text is this : ", searchBar.value);
+        searchBar.value = '';
+    }
+
+    searchButton.addEventListener('click', handleSearch);
+    searchBar.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            handleSearch();
+        }
+    });
+
 }
