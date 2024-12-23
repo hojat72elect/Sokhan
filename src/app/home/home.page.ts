b8fec3d2da149c9869ba3dc0ffd924a2d997a7f6
hojat <hojat72elect@gmail.com>
2024-12-22 20:11:23 -0500

    initial implementation of the data layer completed.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,14 @@ import {
     IonButton
 } from '@ionic/angular/standalone';
 import {FormsModule} from "@angular/forms";
+import {RemoteWordEntry} from "../api/entities/RemoteWordEntry";
+import axios from "axios";
+
+async function fetchRemoteWordEntries(inputWord: string): Promise<RemoteWordEntry[]> {
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${inputWord}`;
+    const apiResponse = await axios.get<RemoteWordEntry[]>(url);
+    return apiResponse.data;
+}
 
 @Component({
     selector: 'app-home',
@@ -23,8 +31,10 @@ export class HomePage {
     constructor() {
     }
 
-    logInputText() {
-        console.log(this.inputText);
+    searchWord() {
+        fetchRemoteWordEntries(this.inputText).then(entries => {
+            console.log(entries[0].meanings[0].definitions[0].definition);
+        })
         this.inputText = '';
     }
 }
