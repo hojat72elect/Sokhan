0d7de7de10d1205ab3050c8ab6d89427f8d06a58
hojat72elect <hojat72elect@gmail.com>
2026-02-26 03:00:14 -0500

    Added the spring boot project (backend)

---
+++ backend/sokhan/src/main/kotlin/ca/hojat/sokhan/SokhanApplication.kt
@@ -0,0 +1,11 @@
+package ca.hojat.sokhan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SokhanApplication
+
+fun main(args: Array<String>) {
+	runApplication<SokhanApplication>(*args)
+}
---
+++ backend/sokhan/src/test/kotlin/ca/hojat/sokhan/SokhanApplicationTests.kt
@@ -0,0 +1,13 @@
+package ca.hojat.sokhan
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SokhanApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
